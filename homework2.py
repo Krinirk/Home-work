fd82e1f1fec7a544e5652cdbea526511cd86ab1b
Krinirk <nikita@empire-a.ru>
2024-05-21 23:27:17 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kol_DZ=12
+kol_hour=1.5
+name_kurs='Python'
+time_1_DZ=float(kol_hour/kol_DZ)
+print('Курс: ',name_kurs,', ','всего задач: ',kol_DZ,', ','затрачено часов: ',kol_hour,', ','среднее время выполнения ',time_1_DZ,' часа.',sep='')
