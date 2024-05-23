ecef04f628666c26f52d317af6e4e82c86902ee4
Krinirk <nikita@empire-a.ru>
2024-05-23 23:46:59 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_list = ['Russia', 'USA', 'Japan', 'China', 'Germany']
+print('List:', my_list)
+print('First country:', my_list[0])
+print('Last country:', my_list[-1])
+print('Sublist:', my_list[2:])
+my_list[2] = 'Italy'
+print('Modified list:', my_list)
+print()
+my_dict = {'Лимон': 'Кислый', 'Шоколад': 'Сладкий', 'Лук': 'Горький', 'Соль': 'Соленый', 'Мясо': 'Умами'}
+print('Словарь:', my_dict)
+print('Вкус:', my_dict['Лимон'])
+my_dict['Лимон'] = 'Очень кислый'
+my_dict['Водоросли'] = 'Умами'
+print('Измененный словарь:', my_dict)
---
+++ homework7.py
@@ -0,0 +1,16 @@
+my_dict={'John':1985, 'Elizabeth':1988,'James':1991,'William':1982,'Kate':1995}
+print('Словарь: ',my_dict)
+print('Присутствует: ', my_dict['James'])
+print('Отсутствует: ', my_dict.get('Ivan'))
+my_dict.update({'Julia':1984,
+               'Din':1992})
+a=my_dict.pop('Din')
+print(a)
+print('Измененный словарь: ',my_dict)
+print()
+my_set={2,2,'c','c',3.5,3.5}
+print('Множество: ',my_set)
+my_set.add(85)
+my_set.add('string')
+my_set.discard(2)
+print('Измененное множество:',my_set)
