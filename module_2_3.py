50ae89f12b32628dbb4508263852bdd9e068d5c9
Krinirk <nikita@empire-a.ru>
2024-05-30 10:49:50 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,24 +1,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-print('Список: ', my_list)
 start = 0
 while start < len(my_list):
-    number = int(input())
     start += 1
-    if number == my_list[start] > 0:
+    if my_list[start] > 0:
         continue
-    # elif number == my_list[1] > 0:
-    #     continue
-    # elif number == my_list[2] > 0:
-    #     continue
-    # elif number == my_list[3] > 0:
-    #     continue
-    else:
-        if number != my_list[start]:
+    elif my_list[start] < 0:
         break
-        
-    # elif number > my_list[0] > 0:
-    #     print(my_list[0])
-    # elif number > my_list[0] > 0:
-    #     print(my_list[0])
-    # elif number > my_list[0] > 0:
-    #     print(my_list[0])
+print(my_list[start])
