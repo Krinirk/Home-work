ef70835c8ed0f75a7b7cbf96da6cc3abd98f8c1c
Krinirk <nikita@empire-a.ru>
2024-06-26 10:53:15 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -1,15 +1,14 @@
 class House:
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+  def __init__(self, name, number_of_floors):
+      self.name = name
+      self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor):
-        self.new_floor = int()
-        if 1 < new_floor > self.number_of_floors:
-            print('Такого этажа не существует')
-        else:
-            for i in range(1, new_floor + 1):
-                print(i)
+  def go_to(self, new_floor):
+      if new_floor > self.number_of_floors or new_floor <= 0:
+          print('Такого этажа не существует')
+      else:
+          for i in range(1, new_floor + 1):
+              print(i)
 
 
 h1 = House('ЖК Горский', 18)
